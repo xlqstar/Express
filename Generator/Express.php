a18cabdd0dccb4d833f74d45fb2f47374cfab693
Xiao Liqun <xlqstar@gmail.com>
2013-01-09 23:44:48 +0800

    增加了zh2PinYin对象，小跨度更新

--- Generator/Express.php
+++ Generator/Express.php
@@ -1,39 +1,51 @@
 <?php
-class Express(){
+class Express{
 
 	var $conf = array();
 	var $initial_depth = 0;
+	var $zh2PinYin = null;
 
 	private function Express(){
 		include('config.php');
 		$this->conf = $config;
+		include('lib\Zh2PinYin.php');
+		$this->zh2PinYin = new Zh2PinYin();
 		$this->initial_depth = substr_count($this->conf['blogSrcDir'],'\\');//初始深度
 	}
 
 	public function generate($blogSrcDir,$blogDestDir){
-		//TODO:判断
-		if($blogDestDir && !is_dir($blogDestDir)){//如果存在目标目录参数并且目标目录不存在则创建该目录
+
+		if(!is_dir($blogDestDir) || !$blogSrcDir){//如果没有获取到参数或者参数
+			log('参数没有输入、或者不正确、或者源路径不为目录，进程已经停止',ERRO);
+		}elseif($blogDestDir && !is_dir($blogDestDir)) {
 			mkdir($blogDestDir);
-		}elseif(!is_dir($blogDestDir)){
-			log("博客源数据目录不存在",ERRO);
-		}elseif(!$blogDestDir || !$blogSrcDir) {
-			log('aruments dosen\'t be given; progress has stop!',ERRO);
 		}
 
-		foreach( glob($blogSrcDir.'\*') as $fullPath ){
+		$fileList = glob($blogSrcDir.'\*');
+
+		foreach( $fileList as $fullPath ){
+
 			$srcObjName = substr(strrchr($fullPath, '\\'), 1);
 			$destObjFullpath = str_replace($blogSrcDir,$blogDestDir,$fullPath);
 			$depth = substr_count($fullPath,'\\') - $this->initial_depth;//当前相对深度
+
 			if (is_dir($fullPath)) {
 				$srcDirName = $srcObjName; $destDirFullpath = $destObjFullpath;
-				if ($depth == ) {
-					# code...
-				}
+				makeBlog($fullPath);
+			}elseif(is_markdown($fullPath)){	
+				makeBlog($fullPath);
 			}else{
-
+				log("存在未预料到的文件：".$fullPath,WARN);
 			}
 		}
 
+	}
+
+	private function 
+
+	private function is_markdown($fullFileName){
+		$suffix = substr(strrchr($fullFileName, '.'), 1)
+		return  $suffix == "md" || $suffix == "markdown";
 	}
 
 	private function makeBlog(){
